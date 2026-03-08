3361ba91024267185bf14560dd6daadf5a72dfe1
yashvid17-pixel <yashvid17@gmail.com>
2026-03-08 14:40:33 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+height_1=float(input("enter the height of the first tree"))
+height_2=float(input("enter the height of the second tree"))
+height_3=float(input("enter the height of the third tree"))
+height_4=float(input("enter the height of the fourth tree"))
+height_5=float(input("enter the height of the fifth tree"))
+
+print((height_1+height_2+height_3+height_4+height_5)/5)
